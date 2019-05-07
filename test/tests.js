5975d4b280a5e7a74617e8ca587e285135d62364
Alexandre Laroche <iamtherock2000@gmail.com>
2019-05-07 14:39:40 -0700

    Fits

--- test/tests.js
+++ test/tests.js
@@ -24,6 +24,7 @@ describe('server.js', function () {
             .expect(200)
             .end((err, res) => {
                 assert.equal(res.status, 302);
+                console.log(res.header);
                 let sess = res.header["set-cookie"] !== undefined;
                 assert.equal(sess, true);
                 done();
@@ -48,15 +49,16 @@ describe('server.js', function () {
     });
     it('/login should give you a sessionID', function (done) {
         body = {};
-        body.email = "ahmad1@nikko.com";
-        body.pwd = "Asdf12345";
+        body.email = "alex@gmail.com";
+        body.pwd = "Alexalex123";
         server
             .post('/login')
             .send(body)
             .expect(200)
             .end((err, res) => {
                 assert.equal(res.status, 302);
-                let sess = res.headers["set-cookie"] !== undefined;
+                console.log(res.headers);
+                let sess = res.headers["location"] === '/home';
                 assert.equal(sess, true);
                 done();
             });
@@ -64,19 +66,68 @@ describe('server.js', function () {
 
     it('/shop should have status 200', (done)=>{
         body = {};
+        body.email = "alex@gmail.com";
+        body.pwd = "Alexalex123";
+        server
+            .post('/login')
+            .send(body)
+            .expect(200)
+            .end((err, res) => {
+                console.log(res.headers);
+                server
+                    .get('/shop')
+                    .expect(200)
+                    .end((err,res1) => {
+                        assert.equal(res1.status, 200);
+                        // console.log(res1.res.text);
+                        if (res1.res.text.includes('Add to cart')){
+                            sess = 1
+                        }else {
+                            sess = 0
+                        }
+                        assert.equal(sess, 1);
+                        done();
+                    })
+            })
+
+    });
+
+    it('/my_cart should have status 200', (done)=>{
+        body = {};
         body.email = "example@example.com";
         body.pwd = "Asdf123!@#";
         server
             .post('/login')
             .send(body);
-            server
-                .get('/shop')
-                .expect(200)
-                .end((err,res) => {
-                    assert.equal(res.status, 200);
-                    let sess = res.headers['userId'] === 'example@example.com';
-                    assert.equal(sess, true);
-                    done();
-                })
-    })
+        server
+            .get('/my_cart')
+            .expect(200)
+            .end((err,res) => {
+                assert.equal(res.status, 302);
+                let sess = res.session.userId === 'example@example.com';
+                assert.equal(sess, true);
+                done();
+            })
+    });
+
+
+
+    it('TRIAL TYPE BEAT', function (done) {
+        body = {};
+        body.email = "ahmad123wqe"+String((Math.random()*100000)+1)+"1@nikko2.com";
+        body.pwd = 'Asdf12345';
+        body.pwd2 = 'Asdf12345';
+        server
+            .post('/register')
+            .send(body)
+            .expect(200)
+            .end((err, res) => {
+                assert.equal(res.status, 302);
+                console.log(res.header);
+                let sess = res.header["set-cookie"] !== undefined;
+                assert.equal(sess, true);
+                done();
+            });
+    });
 });
+
